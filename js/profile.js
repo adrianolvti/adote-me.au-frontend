af717a9e0555cd8bdd4fe6cd35ad614899f2f83f
KevinBitencourt <53475402+KevinBitencourt@users.noreply.github.com>
2023-01-14 12:08:24 -0300

    AMA-55: Criando tabelas

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,49 @@
 // Solicitação GET.
-fetch('http://http://localhost:8080/')
+fetch('http://localhost:8080/animal/all-by-user')
     // Tratamento do sucesso
     .then(response => response.json())  // converter para json
     .then(json => console.log(json))    //imprimir dados no console
     .catch(err => console.log('Erro de solicitação', err)); // lidar com os erros por catch
+
+    function allAnimals(url) {
+    let request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send();
+    return request.responseText;
+}
+
+function newLineTable(animal) {
+    let line = document.createElement("tr");
+    let tdId = document.createElement("td");
+    let tdName = document.createElement("td");
+    let tdType = document.createElement("td");
+    let tdAdopted = document.createElement("td");
+   
+
+    tdId.innerHTML = animal.id;
+    tdName.innerHTML = animal.name;
+    tdType.innerHTML = animal.type;
+    tdAdopted.innerHTML = animal.adopted;
+    
+
+    line.appendChild(tdId);
+    line.appendChild(tdName);
+    line.appendChild(tdType);
+    line.appendChild(tdAdopted);
+    
+
+    return line;
+}
+
+function main() {
+    let data = allAnimal("https://localhost:8080/animal/");
+    let animals = JSON.parse(data);
+    let table = document.getElementById("table");
+
+    animals.forEach(animal => {
+        let line = newLineTable(animal);
+        table.appendChild(line);
+    });
+}
+
+main()
