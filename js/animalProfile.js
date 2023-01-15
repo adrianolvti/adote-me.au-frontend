db794e1482657ece897fea2050da74edc08df44d
MarcioMengerS <gm717gem@gmail.com>
2023-01-15 19:52:40 -0300

    Task-63: Tela perfil do animal

---
+++ js/animalProfile.js
@@ -0,0 +1,50 @@
+/////////////////Lista dados do animal selecionado na página principal/////////////////////////////
+var params = window.location.search.substring(1);
+console.log(params);
+let mail;
+fetch('http://localhost:8080/animal/'+params)
+    .then(response => response.json())
+    .then(data => {
+            const block= document.getElementById('block');
+            block.setAttribute('data-id', data.profileImage.imgurId);
+
+            const perfil = document.getElementById('perfil');
+            
+            const nome = document.createElement('h4');
+            nome.textContent = 'Nome: '+data.name;
+
+            const raca = document.createElement('h4');
+            raca.textContent= 'Raça: '+data.breed.type;
+            
+            const idade = document.createElement('h4');
+            idade.textContent= 'Idade: '+ data.age +' ano(s)';
+
+            const carac = document.createElement('h4');
+            carac.textContent = 'Características Físicas: '+ data.physicalCharacteristics;
+
+            const desc = document.createElement('h4');
+            desc.textContent = 'Descrição: '+ data.description;
+
+            const doador = document.createElement('h4');
+            doador.textContent= 'Nome do Doador: '+ data.user.name;
+
+            const email = document.createElement('h4');
+            email.textContent= 'Forma de Contato: '+ data.user.email;
+            mail = data.user.email;
+
+            perfil.appendChild(nome);
+            perfil.appendChild(raca);
+            perfil.appendChild(idade);
+            perfil.appendChild(carac);
+            perfil.appendChild(desc);
+            perfil.appendChild(doador);
+            perfil.appendChild(email);
+        })
+    .catch(err => {
+        console.log(err)
+    });
+
+function mostraEmail(){
+    console.log(mail);
+    document.getElementById('email').innerHTML = mail;
+}
