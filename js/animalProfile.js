592c04f9265053a9c7085b28b90a5281e008e714
joao bittencourt <joaogsbittencourt@gmail.com>
2023-01-17 00:53:04 -0300

    TASK-56: Criar tela de cadastro de animais

--- js/animalProfile.js
+++ js/animalProfile.js
@@ -1,50 +1,58 @@
 /////////////////Lista dados do animal selecionado na página principal/////////////////////////////
 var params = window.location.search.substring(1);
-console.log(params);
+
 let mail;
-fetch('http://localhost:8080/animal/'+params)
+fetch('http://localhost:8080/animal/' + params)
     .then(response => response.json())
     .then(data => {
-            const block= document.getElementById('block');
-            block.setAttribute('data-id', data.profileImage.imgurId);
 
-            const perfil = document.getElementById('perfil');
-            
-            const nome = document.createElement('h4');
-            nome.textContent = 'Nome: '+data.name;
+        const block = document.getElementById('block');
+        imgurId = 'ygf81Ww';
 
-            const raca = document.createElement('h4');
-            raca.textContent= 'Raça: '+data.breed.type;
-            
-            const idade = document.createElement('h4');
-            idade.textContent= 'Idade: '+ data.age +' ano(s)';
+        if (data.profileImage != null) {
+            console.log(typeof data.profileImage);
+            imgurId = data.profileImage.imgurId;
+        }
 
-            const carac = document.createElement('h4');
-            carac.textContent = 'Características Físicas: '+ data.physicalCharacteristics;
+        block.setAttribute('data-id', imgurId);
 
-            const desc = document.createElement('h4');
-            desc.textContent = 'Descrição: '+ data.description;
+        const perfil = document.getElementById('perfil');
 
-            const doador = document.createElement('h4');
-            doador.textContent= 'Nome do Doador: '+ data.user.name;
+        const nome = document.createElement('h4');
+        nome.textContent = 'Nome: ' + data.name;
 
-            const email = document.createElement('h4');
-            email.textContent= 'Forma de Contato: '+ data.user.email;
-            mail = data.user.email;
+        const raca = document.createElement('h4');
+        raca.textContent = 'Raça: ' + data.breed.type;
 
-            perfil.appendChild(nome);
-            perfil.appendChild(raca);
-            perfil.appendChild(idade);
-            perfil.appendChild(carac);
-            perfil.appendChild(desc);
-            perfil.appendChild(doador);
-            perfil.appendChild(email);
-        })
+        const idade = document.createElement('h4');
+        idade.textContent = 'Idade: ' + data.age + ' ano(s)';
+
+        const carac = document.createElement('h4');
+        carac.textContent = 'Características Físicas: ' + data.physicalCharacteristics;
+
+        const desc = document.createElement('h4');
+        desc.textContent = 'Descrição: ' + data.description;
+
+        const doador = document.createElement('h4');
+        doador.textContent = 'Nome do Doador: ' + data.user.name;
+
+        const email = document.createElement('h4');
+        email.textContent = 'Forma de Contato: ' + data.user.email;
+        mail = data.user.email;
+
+        perfil.appendChild(nome);
+        perfil.appendChild(raca);
+        perfil.appendChild(idade);
+        perfil.appendChild(carac);
+        perfil.appendChild(desc);
+        perfil.appendChild(doador);
+        perfil.appendChild(email);
+    })
     .catch(err => {
         console.log(err)
     });
 
-function mostraEmail(){
+function mostraEmail() {
     console.log(mail);
     document.getElementById('email').innerHTML = mail;
 }
